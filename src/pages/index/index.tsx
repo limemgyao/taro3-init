5555b346c2a772f25a256f05f211790486c8aea3
lixia <lixia123>
2021-12-09 15:11:21 +0800

    33

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -19,14 +19,7 @@ export default class Index extends Component {
 
   render () {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
-      </View>
+      <View className='index'>哈喽，欢迎来到我的世界</View>
     )
   }
 }
