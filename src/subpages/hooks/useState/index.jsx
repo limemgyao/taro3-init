c70e4655ad4fb445742d80cd0ebab41a6bb3b08d
lixia <lixia123>
2022-02-15 17:37:46 +0800

    setJson 赋值里面做判断写法可行

--- src/subpages/hooks/useState/index.jsx
+++ src/subpages/hooks/useState/index.jsx
@@ -1,13 +1,27 @@
-import { Component, useState, useEffect, useReducer} from 'react'
+import {Component, useState, useEffect, useReducer} from 'react'
 import {Button, View} from '@tarojs/components'
 import './index.scss'
 
 export default () => {
+  const [num, setNum] = useState(0)
 
-  const [num,setNum] = useState(0)
+
+  // useEffect 用法
+  const a = {name: '1', sex: '男'}
+  const b = {name: '2', sex: '男'}
+  const [json, setJson] = useState(JSON.stringify(a))
+
+  const isJson = (m) => {
+    if (typeof m == 'object' && JSON.stringify(m).indexOf('{') == 0) {
+      return true
+    }
+    return false
+  }
+
   useEffect(() => {
-
-  }, [])
+    setJson(isJson(b) ? b : '')
+    console.log('json----------', json)
+  }, [num])
 
   return (
     <View className='index'>
@@ -21,4 +35,5 @@ export default () => {
       </View>
     </View>
   )
+
 }
